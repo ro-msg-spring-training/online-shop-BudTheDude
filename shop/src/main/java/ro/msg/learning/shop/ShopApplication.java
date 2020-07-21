5e567a0356a1522c98b7e0d82b6e482049a6569b
BudTheDude <bogdan_bud98@yahoo.com>
2020-07-21 18:40:29 +0300

    Configured H2 and Tested Flyway Migration

--- shop/src/main/java/ro/msg/learning/shop/ShopApplication.java
+++ shop/src/main/java/ro/msg/learning/shop/ShopApplication.java
@@ -1,13 +1,19 @@
 package ro.msg.learning.shop;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
 
 @SpringBootApplication
-public class ShopApplication {
+public class ShopApplication  {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopApplication.class, args);
+
 	}
+
 
 }
